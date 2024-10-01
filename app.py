c3b789c00e730258df8adcc2711dd0e2af01687f
Gunkirat15 <gunkirat9@gmail.com>
2024-10-01 17:16:00 -0600

    app updated

--- app.py
+++ app.py
@@ -1,15 +1,55 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf) #creates a pdf object that has pages
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(raw_text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size = 1000,
+        chunk_overlap = 200,
+        length_function = len
+    )
+    chunks = text_splitter.split_text(raw_text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts = text_chunks, embedding = embeddings)
+    return vectorstore
+
 
 def main():
-    st.set_page_config(page_title="Chat with Muttiple PDFs", page_icon=":books:")
+    load_dotenv()
+    st.set_page_config(page_title="Chat with Multiple PDFs", page_icon=":books:")
 
-    st.header("Chat with Multiple PDFs :books")
+    st.header("Chat with Multiple PDFs :books:")
     st.text_input("Ask a question about your documents:")
 
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your files here and click on 'Process'")
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload your files here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):   #shows a spinner making it user friendly
+                #get the raw text from pdfs
+                raw_text = get_pdf_text(pdf_docs)
+
+                #get the text chunks from raw text
+                text_chunks = get_text_chunks(raw_text)
+
+                vectorstore = get_vector_store(text_chunks)
+                
+
 
 if __name__ == "__main__":
     main()
